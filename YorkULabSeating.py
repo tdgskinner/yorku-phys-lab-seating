965d78e8c350e62ddd226dfd6f33f5edacd06c76
mkareem <mohammad.kareem@cern.ch>
2022-10-25 11:25:40 -0400

    add setting memory

--- YorkULabSeating.py
+++ YorkULabSeating.py
@@ -1,6 +1,7 @@
 import sys , os
 from PyQt6 import QtWidgets, QtCore
 from PyQt6 import uic
+from PyQt6.QtCore import QSettings
 
 import logging
 import scripts.SeatingManager as seating
@@ -38,6 +39,25 @@ class OutputWrapper(QtCore.QObject):
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
+        # To load the last user setting from previous session
+        self.default_settings = {
+            'year':'2022', 
+            'semester':'Fall',
+            'code':'2213',
+            'coursename':'PHYS',
+            'session':'LAB 02',
+            'ta_name':'Mohammad Kareem',
+            'hostname':'127.0.0.1',
+            'portnumber':'5000',
+            'data_dir':'data',
+            'exp_csv_path':'exp_autogen_list.csv',
+            'stud_csv_path':'student_autogen_list.csv',
+            'exp_id':1,
+            'n_group':6,
+            'n_benches':4,
+        }
+        self.getSettingValues()
+        #-------------------
         QtWidgets.QMainWindow.__init__(self)
         self.ui = uic.loadUi('YorkULabSeating.ui',self)
 
@@ -46,42 +66,71 @@ class MainWindow(QtWidgets.QMainWindow):
         stderr = OutputWrapper(self, False)
         stderr.outputWritten.connect(self.handleOutput)
         
-        self.year       = self.lineEdit_year.text() 
-        self.semester   = self.comboBox_semester.currentText()
-        self.code       = self.lineEdit_code.text() 
-        self.coursename = self.lineEdit_coursename.text() 
-        self.session    = self.lineEdit_session.text()
-        self.TA_name    = self.lineEdit_ta.text() 
-        self.n_group    = int(self.lineEdit_ngroups.text())
-        self.n_benches    = int(self.lineEdit_nbenches.text())
+        self.semester   = self.setting_Course.value('semester')
+        self.year       = self.setting_Course.value('year')
+        self.code       = self.setting_Course.value('code')
+        self.coursename = self.setting_Course.value('coursename')
+        self.session    = self.setting_Course.value('session')
+        self.TA_name    = self.setting_Course.value('ta_name')
+        self.hostname   = self.setting_Network.value('hostname')
+        self.portnumber = self.setting_Network.value('portnumber')
+        self.data_dir   = self.setting_Course.value('data_dir')
+        self.exp_csv_path  = self.setting_Course.value('exp_csv_path')
+        self.stud_csv_path = self.setting_Course.value('stud_csv_path')
+        self.exp_id = self.setting_Course.value('exp_id')
+        self.n_group    = self.setting_Course.value('n_group')
+        self.n_benches  = self.setting_Course.value('n_benches')       
+
+        # set default values if there was no previous setting
+        if not self.semester: self.semester = self.default_settings['semester']
+        if not self.year: self.year = self.default_settings['year']
+        if not self.code: self.code = self.default_settings['code']
+        if not self.coursename: self.coursename = self.default_settings['coursename']
+        if not self.session: self.session = self.default_settings['session']
+        if not self.TA_name: self.TA_name = self.default_settings['ta_name']
+        if not self.hostname: self.hostname = self.default_settings['hostname']
+        if not self.portnumber: self.portnumber = self.default_settings['portnumber']
+        if not self.data_dir: self.data_dir = self.default_settings['data_dir']
+        if not self.exp_csv_path: self.exp_csv_path = self.default_settings['exp_csv_path']
+        if not self.stud_csv_path: self.stud_csv_path = self.default_settings['stud_csv_path']
+        if not self.exp_id: self.exp_id = self.default_settings['exp_id']
+        if not self.n_group: self.n_group = self.default_settings['n_group']
+        if not self.n_benches: self.n_benches = self.default_settings['n_benches']
         
-
-        self.hostname   = self.lineEdit_host.text()
-        self.portnumber = self.lineEdit_port.text()
-
-        self.data_dir   = self.lineEdit_data_dir.text()
-
-        self.exp_csv_path = os.path.join('scripts', self.data_dir, self.lineEdit_exp_csv.text() )
-        self.stud_csv_path = os.path.join('scripts', self.data_dir, self.lineEdit_stud_csv.text() )
+        self.lineEdit_ta.setText(self.TA_name)
+        self.lineEdit_year.setText(self.year) 
+        self.comboBox_semester.setCurrentText(self.semester)
+        self.lineEdit_code.setText(self.code) 
+        self.lineEdit_coursename.setText(self.coursename) 
+        self.lineEdit_session.setText(self.session)
+        self.lineEdit_ngroups.setText(str(self.n_group))
+        self.lineEdit_nbenches.setText(str(self.n_benches))
+        self.lineEdit_host.setText(self.hostname)
+        self.lineEdit_port.setText(self.portnumber)
+        self.lineEdit_data_dir.setText(self.data_dir)
+        self.spinBox_exp_id.setValue(self.exp_id)
+        self.lineEdit_exp_csv.setText(self.exp_csv_path)
+        self.lineEdit_stud_csv.setText(self.stud_csv_path)
+        
+        self.exp_csv_path = os.path.join('scripts', self.data_dir, self.exp_csv_path )
+        self.stud_csv_path = os.path.join('scripts', self.data_dir, self.stud_csv_path )
 
         self.lineEdit_pkl.setEnabled(False)
         self.pkl_file_name   = self.set_pklfile_name()
 
-        self.exp_id     = self.spinBox_exp_id.value()
-        
         self.thread={}
 
         #--slots
         self.pushButton_save_settings.clicked.connect(self.save_button_click)
-        #self.pushButton_grouping.clicked.connect(lambda: seating.make_groups(self.exp_csv_path, self.stud_csv_path, self.session, self.n_group, self.pkl_file_name ))
         self.pushButton_grouping.clicked.connect(self.generate_groups)
-        
         self.pushButton_htmlgen.clicked.connect(self.check_pkl)
-        
         self.spinBox_exp_id.valueChanged.connect(self.set_spin_value)
-        
         self.pushButton_start_webserver.clicked.connect(self.start_webserver_worker)
         self.pushButton_stop_webserver.clicked.connect(self.stop_webserver_worker)
+    
+    def getSettingValues(self):
+        self.setting_Course = QSettings('YorkLabSeating', 'Course')
+        self.setting_Network = QSettings('YorkLabSeating', 'Network')
     
     def save_button_click(self):
         self.year       = self.lineEdit_year.text() 
@@ -178,6 +227,22 @@ class MainWindow(QtWidgets.QMainWindow):
         button = dlg.exec()
 
         if button == QtWidgets.QMessageBox.StandardButton.Yes:
+            #--- store the current setting in the system before closing the app
+            self.setting_Course.setValue('ta_name', self.lineEdit_ta.text())
+            self.setting_Course.setValue('year', self.lineEdit_year.text() )
+            self.setting_Course.setValue('semester', self.comboBox_semester.currentText())
+            self.setting_Course.setValue('code', self.lineEdit_code.text() )
+            self.setting_Course.setValue('coursename', self.lineEdit_coursename.text() )
+            self.setting_Course.setValue('session', self.lineEdit_session.text())
+            self.setting_Network.setValue('hostname', self.lineEdit_host.text())
+            self.setting_Network.setValue('portnumber', self.lineEdit_port.text())
+            self.setting_Course.setValue('data_dir', self.lineEdit_data_dir.text())
+            self.setting_Course.setValue('exp_csv_path', self.lineEdit_exp_csv.text())
+            self.setting_Course.setValue('stud_csv_path', self.lineEdit_stud_csv.text())
+            self.setting_Course.setValue('exp_id', int(self.spinBox_exp_id.value())  )
+            self.setting_Course.setValue('n_group', int(self.lineEdit_ngroups.text()) )
+            self.setting_Course.setValue('n_benches', int(self.lineEdit_nbenches.text()))
+            #---
             event.accept()
         else:
             event.ignore()
