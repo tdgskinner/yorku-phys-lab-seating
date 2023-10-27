42d010885b41c2a39573810cff95ad01bed402d2
Mohammad Kareem <mkareem@yorku.ca>
2023-10-26 21:28:45 -0400

    wip

--- YorkULabSeating.py
+++ YorkULabSeating.py
@@ -8,8 +8,10 @@ from PyQt6.QtWidgets import QDialog, QApplication, QFileDialog, QWidget, QProgre
 from PyQt6.QtWidgets import  QLabel, QVBoxLayout, QComboBox
 from PyQt6.QtGui import QIcon, QPixmap, QFont, QPainter, QPageSize, QPageLayout, QShortcut, QKeySequence, QDesktopServices
 from PyQt6.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QHBoxLayout, QLabel, QLineEdit, QMessageBox, QFileDialog, QTextEdit
 
 from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene
+import shutil
 
 import scripts.SeatingManager as seating
 import scripts.GPcManager as gpc
@@ -46,7 +48,6 @@ class OutputWrapper(QObject):
                 sys.stderr = self._stream
         except AttributeError:
             pass
-
 #--------------------------------------------------------------------------------
 class LabLayoutWindow_new(QWidget):
     def __init__(self, layout_out, main_window_size=QSize(800, 600)):
@@ -88,10 +89,6 @@ class LabLayoutWindow_new(QWidget):
     def resizeEvent(self, event):
         # Resize the image to fit the view when the window is resized
         self.view.fitInView(self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
-
-
-
-
 #--------------------------------------------------------------------------------
 class LabLayoutWindow(QWidget):
     """
@@ -248,19 +245,6 @@ class AttWindow(QWidget):
         for i in range(4, len(self.df.columns)):
             self.tableView_att.setColumnWidth(i,60)
     
-    '''def print_att(self):
-        printer = QPrinter(QPrinter.PrinterMode.PrinterResolution)
-        printer.setPageSize(QPageSize(QPageSize.PageSizeId.Letter))
-
-        #To fix: setPageMargins is doing nothing!
-        printer.setPageMargins(QMarginsF(1,1,1,1), units=QPageLayout.Unit.Millimeter)
-
-        print_dlg = QPrintDialog(printer, self)
-
-        if print_dlg.exec() == QPrintDialog.accepted:
-            print_dlg.paintRequested.connect(self.print_prev_att)
-    '''
-    
     
     def print_prev_dlg(self):
         printer = QPrinter(QPrinter.PrinterMode.PrinterResolution)
@@ -295,8 +279,140 @@ class AttWindow(QWidget):
         painter.end()
         logging.debug('Printing completed')
 
-
 #--------------------------------------------------------------------------------
+class lpc_remote_files(QWidget):
+    def __init__(self, laptop_list):
+        super().__init__()
+
+        self.setWindowTitle("File Copier")
+        self.setGeometry(100, 100, 400, 400)
+
+        #self.central_widget = QWidget(self)
+        #self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+
+        self.browse_button = QPushButton("Browse Files")
+        self.browse_button.clicked.connect(self.browse_files)
+        self.layout.addWidget(self.browse_button)
+
+        self.send_button = QPushButton("Send to Laptops")
+        self.send_button.clicked.connect(self.send_files)
+        self.layout.addWidget(self.send_button)
+
+        client_name = 'SC-L-PH-BC3-ta1'
+        self.client_path = r'\\' + client_name
+        self.destination_label = QLabel("Destination Directory in Client PC:")
+        self.destination_input = QLineEdit('/phys/')
+        self.destination_input.setReadOnly(False)
+        self.layout.addWidget(self.destination_label)
+        self.layout.addWidget(self.destination_input)
+
+        self.selected_files_list = QListWidget(self)
+        self.layout.addWidget(self.selected_files_list)
+
+        # Add QTextEdit for file names and patterns to be deleted
+        self.delete_label = QLabel("File Names/Patterns (e.g. *.pdf) to Delete | one entry per line:")
+        self.delete_input = QTextEdit()
+        self.layout.addWidget(self.delete_label)
+        self.layout.addWidget(self.delete_input)
+
+        # Add button for file deletion
+        self.delete_button = QPushButton("Delete Files")
+        self.delete_button.clicked.connect(self.delete_files)
+        self.layout.addWidget(self.delete_button)
+
+        #self.central_widget.setLayout(self.layout)
+
+        self.selected_files = []
+
+    def browse_files(self):
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        selected_files, _ = dialog.getOpenFileNames(self, "Select Files", "", "")
+
+        if selected_files:
+            self.selected_files.extend(selected_files)
+            self.update_selected_files_list()
+        else:
+            QMessageBox.warning(self, "No Files Selected", "No files were selected to send.")
+
+    def update_selected_files_list(self):
+        self.selected_files_list.clear()
+        for file_path in self.selected_files:
+            file_name = os.path.basename(file_path)
+            item = QListWidgetItem(file_name)
+            item.setFlags(item.flags() | Qt.ItemFlag.ItemIsEditable)
+            self.selected_files_list.addItem(item)
+
+    def send_files(self):
+        if not self.selected_files:
+            QMessageBox.warning(self, "No Files to Send", "No files to send. Please select files first.")
+            return
+
+        destination_path = os.path.join(self.client_path, self.destination_input.text())
+
+        if not destination_path:
+            QMessageBox.warning(self, "No Destination Directory", "Please enter a destination directory.")
+            return
+
+        for source_path in self.selected_files:
+            source_name = os.path.basename(source_path)
+            destination = os.path.join(destination_path, source_name)
+
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+
+            try:
+                shutil.copy(source_path, destination)
+                print(f"Successfully copied file: {source_path} to {destination}")
+            except Exception as e:
+                print(f"Failed to copy file: {source_path} - {e}")
+
+        QMessageBox.information(self, "Task Completed", "All selected files have been copied.")
+
+    
+
+    def removeSelectedFile(self, item):
+        if item:
+            file_name = item.text()
+            self.selected_files = [f for f in self.selected_files if not f.endswith(file_name)]
+            self.update_selected_files_list()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Delete or event.key() == Qt.Key.Key_Backspace:
+            selected_item = self.selected_files_list.currentItem()
+            self.removeSelectedFile(selected_item)
+
+    def delete_files(self):
+        client_name = 'SC-L-PH-BC3-ta1'
+        client_path = r'\\' + client_name
+
+        delete_input = self.delete_input.toPlainText()
+        delete_files = delete_input.splitlines()
+
+        for file in delete_files:
+            file = file.strip()
+            file_to_delete = os.path.join(client_path, self.destination_input.text().strip(), file)
+            
+            if '*' in file:
+                matching_files = glob.glob(file_to_delete)
+                for matching_file in matching_files:
+                    try:
+                        os.remove(matching_file)
+                        print(f"Successfully deleted file: {matching_file}")
+                    except Exception as e:
+                        print(f"Failed to delete file: {matching_file} - {e}")
+            else:
+                try:
+                    os.remove(file_to_delete)
+                    print(f"Successfully deleted file: {file_to_delete}")
+                except Exception as e:
+                    print(f"Failed to delete file: {file_to_delete} - {e}")
+
+        QMessageBox.information(self, "Deletion Completed", "All selected files have been deleted.")
+
+#================================================================================
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         # Default settings will be set if no stored settings found from previous session
@@ -429,6 +545,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pushButton_copyfiles.clicked.connect(self.start_copyfiles_worker)
         self.pushButton_rebootPCs.clicked.connect(self.start_gpc_reboot_worker)
         self.pushButton_rebootLaptops.clicked.connect(self.start_laptop_reboot_worker)
+        self.pushButton_lpc_remote_files.clicked.connect(self.open_lpc_remote_files)
+
+
         self.pushButton_course_dir_browse.clicked.connect(self.browse_course_dir)
         self.pushButton_pc_browse.clicked.connect(self.browse_pc_dir)
         self.checkBox_debugMode.toggled.connect(self.set_debug_mode)
@@ -849,6 +968,12 @@ class MainWindow(QtWidgets.QMainWindow):
                 dlg.exec()
                 return
     
+    def open_lpc_remote_files(self):
+        self.lpc_remote = lpc_remote_files(self.laptop_list)
+        self.lpc_remote.setWindowTitle('Laptops Remote Files Manager')
+        self.lpc_remote.show()
+    
+
     def pc_reboot_setProgress(self, pc_progress):
         self.pc_reboot_pbar.setValue(pc_progress)
 
---
+++ test/send_to_laptops_4.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import shutil
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QHBoxLayout, QLabel, QLineEdit, QMessageBox, QFileDialog, QTextEdit
+from PyQt6.QtCore import Qt
+
+class FileCopierApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("File Copier")
+        self.setGeometry(100, 100, 400, 400)
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+
+        self.browse_button = QPushButton("Browse Files")
+        self.browse_button.clicked.connect(self.browse_files)
+        self.layout.addWidget(self.browse_button)
+
+        self.send_button = QPushButton("Send to Laptops")
+        self.send_button.clicked.connect(self.send_files)
+        self.layout.addWidget(self.send_button)
+
+        client_name = 'SC-L-PH-BC3-ta1'
+        self.client_path = r'\\' + client_name
+        self.destination_label = QLabel("Destination Directory in Client PC:")
+        self.destination_input = QLineEdit('/phys/')
+        self.destination_input.setReadOnly(False)
+        self.layout.addWidget(self.destination_label)
+        self.layout.addWidget(self.destination_input)
+
+        self.selected_files_list = QListWidget(self)
+        self.layout.addWidget(self.selected_files_list)
+
+        # Add QTextEdit for file names to be deleted
+        self.delete_label = QLabel("File Names to Delete (enter one file per line):")
+        self.delete_input = QTextEdit()
+        self.layout.addWidget(self.delete_label)
+        self.layout.addWidget(self.delete_input)
+
+        # Add button for file deletion
+        self.delete_button = QPushButton("Delete Files")
+        self.delete_button.clicked.connect(self.delete_files)
+        self.layout.addWidget(self.delete_button)
+
+        self.central_widget.setLayout(self.layout)
+
+        self.selected_files = []
+
+    def browse_files(self):
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        selected_files, _ = dialog.getOpenFileNames(self, "Select Files", "", "")
+
+        if selected_files:
+            self.selected_files.extend(selected_files)
+            self.update_selected_files_list()
+        else:
+            QMessageBox.warning(self, "No Files Selected", "No files were selected to send.")
+
+    def update_selected_files_list(self):
+        self.selected_files_list.clear()
+        for file_path in self.selected_files:
+            file_name = os.path.basename(file_path)
+            item = QListWidgetItem(file_name)
+            item.setFlags(item.flags() | Qt.ItemFlag.ItemIsEditable)
+            self.addRemoveButton(item)
+            self.selected_files_list.addItem(item)
+
+    def send_files(self):
+        if not self.selected_files:
+            QMessageBox.warning(self, "No Files to Send", "No files to send. Please select files first.")
+            return
+
+        destination_path = os.path.join(self.client_path, self.destination_input.text())
+
+        if not destination_path:
+            QMessageBox.warning(self, "No Destination Directory", "Please enter a destination directory.")
+            return
+
+        for source_path in self.selected_files:
+            source_name = os.path.basename(source_path)
+            destination = os.path.join(destination_path, source_name)
+
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+
+            try:
+                shutil.copy(source_path, destination)
+                print(f"Successfully copied file: {source_path} to {destination}")
+            except Exception as e:
+                print(f"Failed to copy file: {source_path} - {e}")
+
+        QMessageBox.information(self, "Task Completed", "All selected files have been copied.")
+
+    def addRemoveButton(self, item):
+        remove_button = QPushButton("Remove")
+        remove_button.clicked.connect(lambda: self.removeSelectedFile(item))
+
+        layout = QHBoxLayout()
+        layout.addWidget(QLabel(item.text()))
+        layout.addWidget(remove_button)
+
+        custom_widget = QWidget()
+        custom_widget.setLayout(layout)
+
+        self.selected_files_list.setItemWidget(item, custom_widget)
+
+    def removeSelectedFile(self, item):
+        if item:
+            file_name = item.text()
+            self.selected_files = [f for f in self.selected_files if not f.endswith(file_name)]
+            self.update_selected_files_list()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Delete or event.key() == Qt.Key.Key_Backspace:
+            selected_item = self.selected_files_list.currentItem()
+            self.removeSelectedFile(selected_item)
+
+    def delete_files(self):
+        client_name = 'SC-L-PH-BC3-ta1'
+        client_path = r'\\' + client_name
+
+        delete_filenames = self.delete_input.toPlainText().splitlines()
+
+        for filename in delete_filenames:
+            file_to_delete = os.path.join(client_path, self.destination_input.text().strip(), filename.strip())
+
+            try:
+                os.remove(file_to_delete)
+                print(f"Successfully deleted file: {file_to_delete}")
+            except Exception as e:
+                print(f"Failed to delete file: {file_to_delete} - {e}")
+
+        QMessageBox.information(self, "Deletion Completed", "All selected files have been deleted.")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FileCopierApp()
+    window.show()
+    sys.exit(app.exec())
